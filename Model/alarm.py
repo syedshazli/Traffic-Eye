3da81ed93409354647a19aa82baa000f5a61ea2f
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-10-12 15:09:57 -0400

    Update alarm.py

--- Model/alarm.py
+++ Model/alarm.py
@@ -10,8 +10,7 @@ def soundAlarm():
     duration = 3000 # 3000 ms
     freq = 440 #Hz
     winsound.Beep(freq, duration)
-    
-soundAlarm()
+
 def overlap(start1, end1, start2, end2):
     """
     Determine if two line segments overlap.
@@ -28,16 +27,15 @@ def overlap(start1, end1, start2, end2):
     def ccw(A, B, C):
         return (C[1] - A[1]) * (B[0] - A[0]) > (B[1] - A[1]) * (C[0] - A[0])
     
-    def intersect(A, B, C, D):
-        # Example: StartPoint1 is on left of startPoint2. But at the endpoints, startpoint1 is on the right of endpoint2. This means somewhere, an intersection occured
-        return ccw(A, C, D) != ccw(B, C, D) and ccw(A, B, C) != ccw(A, B, D)
+    def intersect(start1, end1, start2, end2):
+        return ccw(start1, start2, end2) != ccw(end1, start2, end2) and ccw(start1, end1, start2) != ccw(start1, end1, end2)
     
     return intersect(start1, end1, start2, end2)
+
 
 # Test the function
 # start1, end1 = [2, 1], [4, 4]
 # start2, end2 = [2, 3], [5, 1]
-# THE EXPECTED OUTPUT IS TRUE
 # print(f"Do the lines overlap? {overlap(start1, end1, start2, end2)}")
 
 # # Additional test cases
@@ -45,3 +43,8 @@ def overlap(start1, end1, start2, end2):
 # print(f"Parallel lines: {overlap([0, 0], [2, 2], [1, 0], [3, 2])}")
 # print(f"Touching endpoints: {overlap([0, 0], [2, 2], [2, 2], [4, 4])}")
 # print(f"One point on the other line: {overlap([0, 0], [4, 4], [2, 2], [4, 0])}")
+
+# Test the soundAlarm if true
+# if overlap(start1,end1, start2, end2) is True:
+#     soundAlarm()
+
