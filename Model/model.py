207ad71a095dc4fa060b845a789ad23b2d1c4fef
Joey Marcinowski <66542880+Pixels128@users.noreply.github.com>
2024-10-12 01:29:57 -0400

    changes

--- Model/model.py
+++ Model/model.py
@@ -1,1 +1,23 @@
-i
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+
+
+def webcam_photo():
+    print(1)
+    ret, f = cap.read()
+    if not ret:
+        raise ValueError("Camera error :(")
+    return f
+
+
+prev_frames = []
+while True:
+    img = webcam_photo()
+    print(img)
+    img = rescale(img)
+#     prev_frames.append(make_frame(img))
+#     sound_alarm = predict(prev_frames[-24])
+#     if sound_alarm:
+#         alarm()
