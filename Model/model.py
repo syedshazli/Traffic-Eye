98e1da06aef59bec9575f1d85a68a760065476f6
Joseph Marcinowski <59673635+jmarcinowski@users.noreply.github.com>
2024-10-12 02:35:00 -0400

    Update model.py

--- Model/model.py
+++ Model/model.py
@@ -1,23 +1,37 @@
+#import libraries of python opencv
 import cv2
 
-cap = cv2.VideoCapture(0)
+# capture video/ video path
+cap = cv2.VideoCapture('cars.mp4')
 
 
-
-def webcam_photo():
-    print(1)
-    ret, f = cap.read()
-    if not ret:
-        raise ValueError("Camera error :(")
-    return f
+#use trained cars XML classifiers
+car_cascade = cv2.CascadeClassifier('haarcascade_cars.xml')
 
 
-prev_frames = []
+#read until video is completed
 while True:
-    img = webcam_photo()
-    print(img)
-    img = rescale(img)
-#     prev_frames.append(make_frame(img))
-#     sound_alarm = predict(prev_frames[-24])
-#     if sound_alarm:
-#         alarm()
+    #capture frame by frame
+    ret, frame = cap.read()
+    #convert video into gray scale of each frames
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    #detect cars in the video
+    cars = car_cascade.detectMultiScale(gray, 1.1, 3)
+    #cv2.im_write(cars)
+
+    #to draw a rectangle in each cars 
+    for (x,y,w,h) in cars:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+        cv2.imshow('video', frame)
+        crop_img = frame[y:y+h,x:x+w]
+
+     #press Q on keyboard to exit
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        break
+
+
+#release the video-capture object
+cap.release()
+#close all the frames
+cv2.destroyAllWindows()
